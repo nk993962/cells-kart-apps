93effabb56dd9a9d28ffdb4f4385a3d63df9343f
nk993962 <128208286+nk993962@users.noreply.github.com>
2023-08-03 15:48:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ dotenv.config();
 
 //databse config
 connectDB();
+
 // esmodules fix
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -27,7 +28,6 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, './client/build')))
-
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
